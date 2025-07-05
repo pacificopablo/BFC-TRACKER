cee711cba9adc155a5b97c3a3284830c1a604ba5
pacificopablo <pacificopablo30@gmail.com>
2025-07-05 20:33:00 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCN0dBQXoaWh_13lF0ON25otUpeH3OGINQ",
+  authDomain: "bfc-tracker.firebaseapp.com",
+  projectId: "bfc-tracker",
+  storageBucket: "bfc-tracker.firebasestorage.app",
+  messagingSenderId: "127052459580",
+  appId: "1:127052459580:web:ee9af6545f841d171cdd39",
+  measurementId: "G-TJEMF05CH7"
+};
