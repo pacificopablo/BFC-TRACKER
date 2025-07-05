130e071f0f7d7f562bfbdfa14ea5a9cc15cfe289
pacificopablo <pacificopablo30@gmail.com>
2025-07-05 21:04:21 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCN0dBQXoaWh_13lF0ON25otUpeH3OGINQ",
   authDomain: "bfc-tracker.firebaseapp.com",
   projectId: "bfc-tracker",
   storageBucket: "bfc-tracker.firebasestorage.app",
   messagingSenderId: "127052459580",
-  appId: "1:127052459580:web:ee9af6545f841d171cdd39",
-  measurementId: "G-TJEMF05CH7"
+  appId: "1:127052459580:web:ee9af6545f841d171cdd39"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
